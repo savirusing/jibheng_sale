4f045821dfe08cae1ce2bea1bc27f4d1d4cf3c60
savirusing <savirusing@hotmail.com>
2025-06-27 11:56:54 +0700

    Update lib.js

--- html/lib.js
+++ html/lib.js
@@ -5,9 +5,18 @@ let candle_data = [];
 fetch("https://script.google.com/macros/s/AKfycbwQ4WH9Zh6zVrF41DZ0whj4obHLsbOI8UQuuAXBTD8BqyTaGqkbpTPpHvlcqTMSr9Zq/exec")
     .then(res => res.json())
     .then(data => {
+        let column = [];
         for ([key, value] of Object.entries(data)) {
-            console.log(key);
-            console.log(value);
+            if (key == 0) {
+                column = value;
+            }
+            else {
+                let row_data = {};
+                for ([k,v] of Object.entries(row_rec)){
+                    row_data[k] = v
+                }
+                candle_data.push(row_data)
+            }
         }
-        // console.log(data); // แสดงข้อมูลทั้งหมดในชีต
+        console.log(candle_data); // แสดงข้อมูลทั้งหมดในชีต
     });
