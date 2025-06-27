c73fc5f6ca0807a7459a6a6d5518752055f58cf9
savirusing <savirusing@hotmail.com>
2025-06-27 11:47:29 +0700

    try database

--- html/lib.js
+++ html/lib.js
@@ -1,3 +1,9 @@
-function textCenter(text){
-    return { text: text, css: { "text-align": "center" }};
+function textCenter(text) {
+    return { text: text, css: { "text-align": "center" } };
 }
+
+fetch("https://script.google.com/macros/s/AKfycbwQ4WH9Zh6zVrF41DZ0whj4obHLsbOI8UQuuAXBTD8BqyTaGqkbpTPpHvlcqTMSr9Zq/exec")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data); // แสดงข้อมูลทั้งหมดในชีต
+    });
