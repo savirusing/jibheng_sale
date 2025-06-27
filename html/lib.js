319315f253e34b7fa847e2539a445adc6b5f59cc
savirusing <savirusing@hotmail.com>
2025-06-27 13:08:54 +0700

    update

--- html/lib.js
+++ html/lib.js
@@ -1,3 +1,24 @@
 function textCenter(text) {
     return { text: text, css: { "text-align": "center" } };
 }
+
+let candle_data = [];
+fetch("https://script.google.com/macros/s/AKfycbwQ4WH9Zh6zVrF41DZ0whj4obHLsbOI8UQuuAXBTD8BqyTaGqkbpTPpHvlcqTMSr9Zq/exec")
+    .then(res => res.json())
+    .then(data => {
+        let column = [];
+        for ([key, value] of Object.entries(data)) {
+            if (key == 0) {
+                column = value;
+            }
+            else {
+                let row_data = {};
+                for ([k, v] of Object.entries(value)) {
+                    row_data[column[k]] = v
+                }
+                candle_data.push(row_data)
+            }
+        }
+        // console.log(candle_data); // แสดงข้อมูลทั้งหมดในชีต
+        $$("p_candle_table").parse(candle_data);
+    });
--- html/main.js
+++ html/main.js
@@ -43,26 +43,7 @@ webix.ready(function () {
                             ],
                             on: {
                                 onAfterLoad: function () {
-                                    let candle_data = [];
-                                    fetch("https://script.google.com/macros/s/AKfycbwQ4WH9Zh6zVrF41DZ0whj4obHLsbOI8UQuuAXBTD8BqyTaGqkbpTPpHvlcqTMSr9Zq/exec")
-                                        .then(res => res.json())
-                                        .then(data => {
-                                            let column = [];
-                                            for ([key, value] of Object.entries(data)) {
-                                                if (key == 0) {
-                                                    column = value;
-                                                }
-                                                else {
-                                                    let row_data = {};
-                                                    for ([k, v] of Object.entries(value)) {
-                                                        row_data[column[k]] = v
-                                                    }
-                                                    candle_data.push(row_data)
-                                                }
-                                            }
-                                            // console.log(candle_data); // แสดงข้อมูลทั้งหมดในชีต
-                                            $$("p_candle_table").parse(candle_data);
-                                        });
+                                    this.parse(candle_data);
                                 }
                             }
                             // data: [
